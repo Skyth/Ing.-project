3fde354a6e3e86f4eba6e36dd7f6e83dc33bd6b6
Andrew Kozlov <sazor.mail@gmail.com>
2015-05-11 01:31:48 +0300

    merge fix

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,6 +2,6 @@ import DS from 'ember-data';
 import DRFAdapter from 'ember-django-adapter/adapters/drf';
 
 export default DRFAdapter.extend({
-	host: 'http://admin.ingida.ru:80', 
+	host: 'http://admin.ingida.ru:80',
 	namespace: 'api'
 });
